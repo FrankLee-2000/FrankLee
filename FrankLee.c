8ff54a016ebc807b2e18e818083c8918b2557749
FrankLee-2000 <1076362617@qq.com>
2019-10-22 16:24:56 +0800

    prin

--- FrankLee.c
+++ FrankLee.c
@@ -1,6 +1,1 @@
 #include<stdio.h>
-int main(void)
-{
-    printf("I just wanna run!\n");
-    return 0;
-}
---
+++ c13.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+    float x,t,g;
+    t=3,g=10;
+    x=1/2.0*g*t*t;
+    printf("x=%f\n",x)
+    return 0;
+}
